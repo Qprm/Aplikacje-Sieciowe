055beb248e173571e9742792da9d588010c3c27a
Qprm <agata.czopka@gmail.com>
2021-10-23 19:12:29 +0200

    Loan calculator function added

--- app/calc.php
+++ app/calc.php
@@ -1,26 +1,18 @@
 <?php
-// KONTROLER strony kalkulatora
+
 require_once dirname(__FILE__).'/../config.php';
-
-// W kontrolerze niczego nie wysyła się do klienta.
-// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
-// Parametry do widoku przekazujemy przez zmienne.
-
-// 1. pobranie parametrów
 
 $x = $_REQUEST ['x'];
 $y = $_REQUEST ['y'];
 $z = $_REQUEST ['z'];
 
-// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
 
-// sprawdzenie, czy parametry zostały przekazane
 if ( ! (isset($x) && isset($y) && isset($z) )) {
-	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	
 	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
 }
 
-// sprawdzenie, czy potrzebne wartości zostały przekazane
+
 if ( $x == "") {
 	$messages [] = 'Nie podano kwoty';
 }
@@ -31,10 +23,10 @@ if ( $z == "") {
 	$messages [] = 'Nie podano czasu spłaty';
 }
 
-//nie ma sensu walidować dalej gdy brak parametrów
+
 if (empty( $messages )) {
 	
-	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+	
 	if (! is_numeric( $x )) {
 		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
 	}
@@ -49,35 +41,15 @@ if (empty( $messages )) {
 
 }
 
-// 3. wykonaj zadanie jeśli wszystko w porządku
-
-if (empty ( $messages )) { // gdy brak błędów
+if (empty ( $messages )) { 
 	
-	//konwersja parametrów na int
 	$x = intval($x);
 	$y = intval($y);
         $z = intval($z);
 	
         $result = ($x + ($x*($y/100)))/($z*12); 
-	//wykonanie operacji
-	/*switch ($operation) {
-		case 'minus' :
-			$result = $x - $y;
-			break;
-		case 'times' :
-			$result = $x * $y;
-			break;
-		case 'div' :
-			$result = $x / $y;
-			break;
-		default :
-			$result = $x + $y;
-			break;
-	}*/
-        
+	
+      
 }
 
-// 4. Wywołanie widoku z przekazaniem zmiennych
-// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
-//   będą dostępne w dołączonym skrypcie
 include 'calc_view.php';
